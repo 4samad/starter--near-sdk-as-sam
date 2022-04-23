88c437f62b02d28e7473a2953d06e907699fbd1c
AbdulSamad <47020656+4samad@users.noreply.github.com>
2022-04-23 11:24:42 +0400

    initial commit

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -2,7 +2,7 @@ import { storage, Context } from "near-sdk-as"
 
 // return the string 'hello world'
 export function helloWorld(): string {
-  return 'hello world'
+  return 'Hey world, this is Sam contract!'
 }
 
 // read the given key from account (contract) storage
